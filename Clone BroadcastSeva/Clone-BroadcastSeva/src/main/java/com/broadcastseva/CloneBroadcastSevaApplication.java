1b54a3cfd16d76576fc484b09885d046c680f048
vijay verma <91897618+vijay9951@users.noreply.github.com>
2023-01-04 15:52:54 +0000

    added code file

---
+++ Clone BroadcastSeva/Clone-BroadcastSeva/src/main/java/com/broadcastseva/CloneBroadcastSevaApplication.java
@@ -0,0 +1,13 @@
+package com.broadcastseva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneBroadcastSevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneBroadcastSevaApplication.class, args);
+	}
+
+}
---
+++ Clone BroadcastSeva/Clone-BroadcastSeva/src/test/java/com/broadcastseva/CloneBroadcastSevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broadcastseva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneBroadcastSevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
